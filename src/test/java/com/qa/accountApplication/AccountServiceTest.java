9209676139d0fd7453d107bc22fd8cff0fb2dd0a
Nil Montia <nil.montia@academytrainee.com>
2019-04-16 12:43:06 +0100

    Made test to check if class exists and made setUp method

--- src/test/java/com/qa/accountApplication/AccountServiceTest.java
+++ src/test/java/com/qa/accountApplication/AccountServiceTest.java
@@ -1,14 +1,27 @@
 package com.qa.accountApplication;
 
 import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.BeforeClass;
 
 public class AccountServiceTest extends TestCase {
+    private AccountService accountService;
 
+    @Before
     public void setUp() throws Exception {
-        super.setUp();
+        accountService =  new AccountService();
+        accountService.addAccount("Maria", "Watkins", 12);
+        accountService.addAccount("Nil", "Montia", 4);
+        accountService.addAccount("Nil", "Bosch", 1);
+        accountService.addAccount("Maria", "Sellas", 34);
     }
 
     public void tearDown() throws Exception {
+    }
+
+    public void testCreateAccountService(){
+        AccountService accounty=new AccountService();
+        assertNotNull(accounty);
     }
 
     public void testGetMap() {
